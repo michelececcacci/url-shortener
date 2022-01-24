20f27f0f892fe75cfa9897ed5cdf32104381c7da
Michele  Ceccacci <michelececcacci1@gmail.com>
2022-01-24 16:17:13 +0100

    switched to postgres

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,9 +77,13 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : os.environ['DATABASE_NAME'],
+        'USER' : os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST' : 'localhost',
+        'PORT' : '',
     }
 }
 
